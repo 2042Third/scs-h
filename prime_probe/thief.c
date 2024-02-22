f5ef04aa54450387d8387ef3ebb03e0592f712c6
mikeyang <18604713262@163.com>
2024-02-21 22:31:55 -0500

    prime and probe

--- prime_probe/thief.c
+++ prime_probe/thief.c
@@ -72,7 +72,7 @@ bool prime_probe_l2_set(int set, char *buf) {
       printf("Address = %ld, set %d  timing = %d, ((end-start)/16)+8 = %ld \n", lineAddr, set, timing, ((end-start)/16)+8 );
     }
 
-    if(timing > ((end-start)/16)+8 ){
+    if(timing > ((end-start)/16)+6 ){
       found = true;
     }
   }
