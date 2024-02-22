1412fdf1c465ab3233c6ee2669ea9795434fc7c2
mikeyang <18604713262@163.com>
2024-02-21 19:46:04 -0500

    Update

--- flush_reload/thief.c
+++ flush_reload/thief.c
@@ -37,9 +37,8 @@ int flush_reload(int size, uint8_t *buf) {
 //    (*((char *)lineAddr)) ++;
     clflush(lineAddr);
     uint32_t timing = measure_line_access_time(lineAddr);
-    clflush(lineAddr);
     timing += measure_line_access_time(lineAddr);
-    clflush(lineAddr);
+    timing += measure_line_access_time(lineAddr);
     timing += measure_line_access_time(lineAddr);
     if (timing < 500 || i*64 >292000 && i*64 <293000) {
       printf("Address = %ld, set %d  timing = %d\n",lineAddr,i*64, timing);
